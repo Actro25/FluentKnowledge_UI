7a76ccbda130c8820d038b6e2d8eb8dd677a972f
Vergil <skiril502@gmail.com>
2026-05-14 18:53:55 +0300

    feat: initial commit

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FluentKnowledge_UI.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace FluentKnowledge_UI.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
